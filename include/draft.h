1a09e0f3dac58460a35f5966fad589613b4e6fc1
bel-lau <laurenobell@yahoo.com>
2018-10-22 17:18:05 -0400

    Add additional hero configs. Create rough draft data structure.

--- include/draft.h
+++ include/draft.h
@@ -3,12 +3,22 @@
 #define DRAFTER_DRAFT_H
 
 #include <string>
+#include "hero.h"
 
 class Draft
 {
 
   private:
+    std::string draft_name;
+    std::string radiant_name;
+    std::string dire_name;
+    bool radiant_first;
 
+    Hero* radiant_bans[6];
+    Hero* dire_bans[6];
+
+    Hero* radiant_picks[5];
+    Hero* dire_bans[5];
 
   public:
     Draft();
--- src/drafter_module.cpp
+++ src/drafter_module.cpp
@@ -2,7 +2,6 @@
 
 DrafterModule::DrafterModule()
 {
-  std::cout << "Drafter module constructing\n";
   std::string filename = "config/config.yaml";
   if (!checkFile(filename))
   {
@@ -36,18 +35,19 @@ bool DrafterModule::checkFile(std::string file)
 
 void DrafterModule::parseHeroes(YAML::Node heroes)
 {
+  std::cout << "Loading Heroes...\n";
   int count = 0;
   std::string hero_file = "";
   for (YAML::const_iterator it = heroes.begin(); it != heroes.end(); ++it)
   {
     hero_file = "config/heroes/" + it->as<std::string>() + ".yaml";
-    std::cout << hero_file << std::endl;
     if (!initHero(hero_file, count))
     {
       // Hero initialization failed. Panic?
     }
     count++;
   }
+  std::cout << std::endl;
 }
 
 bool DrafterModule::initHero(std::string hero_file, int hero_count)
@@ -55,11 +55,13 @@ bool DrafterModule::initHero(std::string hero_file, int hero_count)
   if (!checkFile(hero_file))
   {
     // File does not exist, exit hero initialization
+    std::cout << "\nHero file not found: " << hero_file << std::endl; 
     return false;
   }
   YAML::Node hero = YAML::LoadFile(hero_file);
 
   this->heroes[hero_count] = hero.as<Hero>();
 
+  std::cout << this->heroes[hero_count].getName() << ", ";
   return true;
 }
